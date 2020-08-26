16c6c303ff1f0338e63f7334a30db8491d05d565
Syntax73 <45923733+Syntax73@users.noreply.github.com>
2020-08-26 10:34:22 -0300

    Criado LessonInput e query para listar todas as lessons

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,22 +1,27 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
 import { LessonService } from './lesson.service';
+import { CreateLessonInput } from './lesson.input';
+import { Lesson } from './lesson.entity';
 
 @Resolver(of => LessonType)
 export class LessonResolver {
   constructor(private lessonService: LessonService) {}
 
   @Query(returns => LessonType)
-  lesson(@Args('id') id: string) {
+  getLesson(@Args('id') id: string): Promise<Lesson> {
     return this.lessonService.getLesson(id);
+  }
+
+  @Query(returns => [LessonType])
+  getAllLessons(): Promise<Lesson[]> {
+    return this.lessonService.getAllLessons();
   }
 
   @Mutation(returns => LessonType)
   createLesson(
-    @Args('name') name: string,
-    @Args('startDate') startDate: string,
-    @Args('endDate') endDate: string,
-  ) {
-    return this.lessonService.createLesson(name, startDate, endDate);
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
+  ): Promise<Lesson> {
+    return this.lessonService.createLesson(createLessonInput);
   }
 }
